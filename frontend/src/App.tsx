df4ee44a5df58a17d2aa2ce91bae7d52f79ff3a1
Alex <15172825+Alex-Coetzee@users.noreply.github.com>
2026-01-17 17:33:44 +0000

    Main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Chat from './components/Chat';
 import CloneSetup from './components/CloneSetup';
+import TaggedUsers from './components/TaggedUsers';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/clone/:username" element={<CloneSetup />} />
         <Route path="/chat/:username" element={<Chat />} />
+        <Route path="/users/:tag" element={<TaggedUsers />} />
       </Routes>
     </Router>
   );
