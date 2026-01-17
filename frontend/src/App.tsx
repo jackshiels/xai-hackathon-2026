57be07497a50250f847b3c4fbe988044e569e6f0
Alex <15172825+Alex-Coetzee@users.noreply.github.com>
2026-01-17 17:55:31 +0000

    Update route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Chat from './components/Chat';
 import CloneSetup from './components/CloneSetup';
-import TagBrowse from './components/TaggedUSers';
+import TagBrowse from './components/TagBrowse';
 
 function App() {
   return (
